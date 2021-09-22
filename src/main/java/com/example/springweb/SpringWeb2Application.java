8ebaac06d5b57fb6c5cfca74cf3b30af57ea085e
medhasalil <medhasalil@gmail.com>
2021-09-22 12:16:15 +0530

    initial commit

---
+++ src/main/java/com/example/springweb/SpringWeb2Application.java
@@ -0,0 +1,14 @@
+package com.example.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeb2Application.class, args);
+		System.out.println("This is a HelloWorld Program");
+	}
+
+}
